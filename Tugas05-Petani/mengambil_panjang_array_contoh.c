6bdc610720c183572da8c881d6c2881d357376a8
arsysysmil <arsyamilht@gmail.com>
2023-10-25 10:55:15 +0700

    file baru pisan

---
+++ Tugas05-Petani/mengambil_panjang_array_contoh.c
@@ -0,0 +1,7 @@
+int array[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+/* ukuran array dalam byte */
+int size = sizeof(array);
+
+/* banyaknya isi array `array` */
+int length = sizeof(array) / sizeof(*array);
